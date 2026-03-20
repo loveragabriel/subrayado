8699a8d8f1e96150aebf0f1ceafaf948aac8a4ce
Gabriel <loveragabriel20@gmail.com>
2026-03-19 22:42:32 -0300

    feat(docker): amend docker-files backend/frontend and docker-compose Set frontend Dockerfile multi-stage Set backend Dockerfile multi-stage Set docker-compose.yml with .env variables

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        canvas: './empty-module.js',
-      },
+  turbopack: {
+    resolveAlias: {
+      canvas: "./empty-module.js",
     },
   },
   webpack: (config) => {
